b022f9c432df0f76059ac0f2679812ef1a363b6f
Kristof Neirynck <k.neirynck@gmail.com>
2021-11-04 13:11:41 +0100

    ValidationError can't be a record because jsp doesn't find getters
    Which leads to this kind of exception:
    SEVERE: Servlet.service() for servlet [jsp.WEB_002dINF.jsp.whiteboards.validation_005ffailed_jsp] threw exception
    jakarta.el.PropertyNotFoundException: Property [errorCode] not found on type [whiteboard.ValidationError]

--- whiteboard/src/main/java/whiteboard/ValidationError.java
+++ whiteboard/src/main/java/whiteboard/ValidationError.java
@@ -1,6 +1,46 @@
 package whiteboard;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
-public record ValidationError(String field, String errorCode) implements Serializable {
+public final class ValidationError implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0L;
+    private final String field;
+    private final String errorCode;
+
+    public ValidationError(String field, String errorCode) {
+        this.field = field;
+        this.errorCode = errorCode;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ValidationError that = (ValidationError) o;
+        return Objects.equals(field, that.field) && Objects.equals(errorCode, that.errorCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(field, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ValidationError{" +
+            "field='" + field + '\'' +
+            ", errorCode='" + errorCode + '\'' +
+            '}';
+    }
 }
